63308a1c9633cd396d0a6c349b4718d86a3b21bd
LLdev-jm <ejungmin7@gmail.com>
2023-09-02 16:56:17 -0500

    files for fruit search project

---
+++ script.js
@@ -0,0 +1,55 @@
+
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+
+function search(str) {
+    str = str.toLowerCase();
+    const results = fruit.filter(fruitName => 
+        fruitName.toLowerCase().indexOf(str) === 0
+    );
+    return results;
+	
+}
+
+function searchHandler(e) {
+    const userInput = e.target.value.toLowerCase().trim();
+    
+    
+    suggestions.innerHTML = '';
+
+    if (userInput !== '') {
+        const searchResults = search(userInput);
+        showSuggestions(searchResults, userInput);
+    } else {
+        suggestions.innerHTML = '';
+    }
+}
+
+function showSuggestions(results, inputVal) {
+    results.forEach(result => {
+        const li = document.createElement('li');
+        li.textContent = result;
+        suggestions.appendChild(li);
+    });
+}
+
+function useSuggestion(e) {
+    if (e.target.tagName === 'LI') {
+
+        const selectedSuggestion = e.target.textContent;
+        input.value = selectedSuggestion;
+
+        suggestions.innerHTML = '';
+    }
+}
+
+input.addEventListener('input', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
+
+
+
+
